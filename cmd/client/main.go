3a983ae9913c53921afc08ca11b2b30c23505acc
Eugene Lepeico <eugenelepeico@gmail.com>
2021-01-12 09:49:41 +0200

    add error to server response

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -11,19 +12,23 @@ import (
 func main() {
 	input, err := os.Open("inputTree.json")
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Unable to open file,", err)
+		fmt.Fprintln(os.Stderr, "Unable to open file:", err)
 		return
 	}
 	defer input.Close()
 
 	t, err := tree.New(input)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Cannot decode tree from that reader,", err)
+		fmt.Fprintln(os.Stderr, "Cannot decode tree from that reader:", err)
 		return
 	}
 
-	if _, err := http.Post("http://localhost:8000/tree", "application/json", &t); err != nil {
-		fmt.Fprintln(os.Stderr, "Cannot send POST request,", err)
+	if resp, err := http.Post("http://localhost:8000/tree", "application/json", &t); err != nil {
+		fmt.Fprintln(os.Stderr, "Cannot exec POST request,", err)
 		return
-	} 
+	} else if resp.StatusCode != 200 {
+		httperr, _ := ioutil.ReadAll(resp.Body)
+		fmt.Fprintln(os.Stderr,"Error from server while executing POST request:", string(httperr))
+		return
+	}
 }
